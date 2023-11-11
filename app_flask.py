681edd5ff3ff814244715dd4a1850fc1d7c9f300
St3an1da4rt <ITstol@yandex.ru>
2023-11-12 04:20:59 +0500

    version.2.0

--- app_flask.py
+++ app_flask.py
@@ -1,10 +1,41 @@
 from flask import Flask, render_template
+from jinja2 import Environment, FileSystemLoader
+import json
+import os
+
+filename_json2 = ''
+direct = os.listdir("json")
+for i in direct:
+    if i.split('.')[1] == 'json':
+        filename_json2 = f"json/{i}"
+
+class_school = {
+    'hy': 'hydrogen-place',
+    'ca': 'carbon-place',
+    'su': 'surfur-place'
+}
+
+with open(filename_json2, 'r') as f:
+    join = json.load(f)
+
+join2 = {j: i for i, j in join.items()}
+
+
+environment = Environment(loader=FileSystemLoader("templates/"))
+template = environment.get_template("index.html")
 
 app = Flask(__name__)
 
+@app.route('/main')
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", join2=join2)
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
